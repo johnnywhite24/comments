db1fabec1192f577cf8ea72fdc1763ada867f097
Baixqiang <bxq@hbyaso.com>
2024-12-12 13:18:07 +0800

    增加发布 调整命令。

--- comment.bat
+++ comment.bat
@@ -1,13 +1,5 @@
 @echo off
 
-REM 检查是否提供了足够的参数
-if "%~1"=="" (
-    echo 用法: %0 <字符串文本参数>
-    exit /b
-)
+java -cp out CommentsMain %CD% %1
 
-REM 执行Java .class文件
-java -cp C:\JohProjects\Other\comments\out\production\comments CommentsMain %CD% %1
-
-REM 等待用户按下任意键以退出脚本
 pause
